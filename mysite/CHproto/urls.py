9862ce72d36faf32801f794f640ffcaf5c353332
MrBen214 <Mrben214@gmail.com>
2017-05-13 15:06:20 -0400

    Add files via upload

--- mysite/CHproto/urls.py
+++ mysite/CHproto/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
 
 
 
-    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/roster/addattendance/$', views.addattendance, name='addattendance'),
-    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/newattendance/$', views.newattendance, name='newattendance'),
+    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/addattendance/$', views.addattendance, name='addattendance'),
+    #url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/newattendance/$', views.newattendance, name='newattendance'),
 
-
+    #url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/graph/$', views.graph, name='graph'),
 
     url(r'^(?P<course_id>[0-9]+)/attendance/$', views.attendance, name='attendance'),
     url(r'^(?P<course_id>[0-9]+)/removecourse/$', views.removecourse, name='removecourse'),
@@ -20,8 +20,10 @@ urlpatterns = [
     url(r'^(?P<course_id>[0-9]+)/roster/newstudent/$', views.newstudent, name='newstudent'),
     url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/editstudent/$', views.editstudent, name='editstudent'),
     url(r'^(?P<course_id>[0-9]+)/roster/$', views.roster, name='roster'),
-    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/attendance/$', views.graphattendancebymonth, name='graphattendancebymonth'),
-    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/attendance/$', views.graphattendancebyweek, name='graphattendancebyweek'),
+    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/graphattendanceoverall/$', views.graphattendanceoverall, name='graphattendanceoverall'),
+    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/(?P<month_number>[0-9]+)/graphattendancebymonth/$', views.graphattendancebymonth, name='graphattendancebymonth'),
+    url(r'^(?P<course_id>[0-9]+)/(?P<month_number>[0-9]+)/graphoverallmonth/$', views.graphoverallmonth, name='graphoverallmonth'),
+    url(r'^(?P<student_id>[0-9]+)/(?P<course_id>[0-9]+)/(?P<week_number>[0-9]+)/graphattendancebyweek/$', views.graphattendancebyweek, name='graphattendancebyweek'),
 
     url(r'^profile/$', views.profile, name='profile'),
     #url(r'^$', views.index, name='index'),
--- mysite/CHproto/views.py
+++ mysite/CHproto/views.py
@@ -148,26 +148,15 @@ def editcourse(request, course_id):
 @login_required
 def addattendance(request, student_id, course_id):
     roster = Roster.objects.get(student=student_id, course=course_id)
+    print("before: " + str(roster))
     course = Course.objects.get(pk=course_id)
     if request.method == "POST":
         form = AttendanceForm(request.POST)
         if form.is_valid():
             attendance = form.save(commit=True)
-            attendance.roster = roster
-            attendance.save()
-            return HttpResponseRedirect('/CHproto/%i/roster/' % course.id)
-    else:
-        form = AttendanceForm()
-    return render(request, 'addattendance.html', {'form': form})
-
-@login_required
-def newattendance(request, student_id, course_id):
-    roster = Roster.objects.get(student=student_id, course=course_id)
-    course = Course.objects.get(pk=course_id)
-    if request.method == "POST":
-        form = AttendanceForm(request.POST)
-        if form.is_valid():
-            attendance = form.save(commit=True)
+            attendance.roster = roster # sets roster value for attendance
+            attendance.save() # saves attendance data.
+            print("after: " + str(attendance.roster))
             return HttpResponseRedirect('/CHproto/%i/roster/' % course.id)
     else:
         form = AttendanceForm()
@@ -189,19 +178,99 @@ def attendance(request, course_id):
     m3_total = {}
     m4_total = {}
 
-    all_totals = {}
+    w1_a = {}
+    w2_a = {}
+    w3_a = {}
+    w4_a = {}
+    w5_a = {}
+    w6_a = {}
+    w7_a = {}
+    w8_a = {}
+    w9_a = {}
+    w10_a = {}
+    w11_a = {}
+    w12_a = {}
+    w13_a = {}
+    w14_a = {}
+    w15_a = {}
+    w16_a = {}
 
+    w1_total = {}
+    w2_total = {}
+    w3_total = {}
+    w4_total = {}
+    w5_total = {}
+    w6_total = {}
+    w7_total = {}
+    w8_total = {}
+    w9_total = {}
+    w10_total = {}
+    w11_total = {}
+    w12_total = {}
+    w13_total = {}
+    w14_total = {}
+    w15_total = {}
+    w16_total = {}
+
+    all_totals = {}
+    week_totals = {}
+
+    m_grand_total = {}
+    w_grand_total = {}
+    grand_total = {}
+
+    m_grand_total = [0,0,0,0]
+    w_grand_total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    grand_total = [0]
+    
     students = []
 
     for roster_entry in roster_entries:
         curr_student = Student.objects.get(pk=roster_entry.student.pk)
         students.append(curr_student)
-
+        print (str(roster_entry.pk))
+        for attend in Attendance.objects.all():
+            print (attend.roster)
         #attendances[curr_student] = Attendance.objects.filter(roster=roster_entry.pk).order_by('date')
         m1_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=1).order_by('date')
         m2_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=2).order_by('date')
         m3_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=3).order_by('date')
         m4_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=4).order_by('date')
+
+        w1_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=1).order_by('date')
+        w2_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=2).order_by('date')
+        w3_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=3).order_by('date')
+        w4_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=4).order_by('date')
+        w5_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=5).order_by('date')
+        w6_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=6).order_by('date')
+        w7_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=7).order_by('date')
+        w8_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=8).order_by('date')
+        w9_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=9).order_by('date')
+        w10_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=10).order_by('date')
+        w11_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=11).order_by('date')
+        w12_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=12).order_by('date')
+        w13_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=13).order_by('date')
+        w14_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=14).order_by('date')
+        w15_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=15).order_by('date')
+        w16_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=16).order_by('date')
+
+        #m1_total[curr_student] = 0
+        w1_total = 0
+        w2_total = 0
+        w3_total = 0
+        w4_total = 0
+        w5_total = 0
+        w6_total = 0
+        w7_total = 0
+        w8_total = 0
+        w9_total = 0
+        w10_total = 0
+        w11_total = 0
+        w12_total = 0
+        w13_total = 0
+        w14_total = 0
+        w15_total = 0
+        w16_total = 0
 
         #m1_total[curr_student] = 0
         m1_total = 0
@@ -209,8 +278,11 @@ def attendance(request, course_id):
         m3_total = 0
         m4_total = 0
 
+        print ("attendances found " + str(len(Attendance.objects.filter(month_number=1))))
+
         for value in m1_a[curr_student]:
             m1_total += value.hours_attended
+            print ("attendances found " )
 
         for value in m2_a[curr_student]:
             m2_total += value.hours_attended
@@ -221,17 +293,94 @@ def attendance(request, course_id):
         for value in m4_a[curr_student]:
             m4_total += value.hours_attended
 
+        for value in w1_a[curr_student]:
+            w1_total += value.hours_attended
+
+        for value in w2_a[curr_student]:
+            w2_total += value.hours_attended
+
+        for value in w3_a[curr_student]:
+            w3_total += value.hours_attended
+
+        for value in w4_a[curr_student]:
+            w4_total += value.hours_attended
+
+        for value in w5_a[curr_student]:
+            w5_total += value.hours_attended
+
+        for value in w6_a[curr_student]:
+            w6_total += value.hours_attended
+
+        for value in w7_a[curr_student]:
+            w7_total += value.hours_attended
+
+        for value in w8_a[curr_student]:
+            w8_total += value.hours_attended
+        
+        for value in w9_a[curr_student]:
+            w9_total += value.hours_attended
+
+        for value in w10_a[curr_student]:
+            w10_total += value.hours_attended
+
+        for value in w11_a[curr_student]:
+            w11_total += value.hours_attended
+
+        for value in w12_a[curr_student]:
+            w12_total += value.hours_attended
+
+        for value in w13_a[curr_student]:
+            w13_total += value.hours_attended
+
+        for value in w14_a[curr_student]:
+            w14_total += value.hours_attended
+
+        for value in w15_a[curr_student]:
+            w15_total += value.hours_attended
+
+        for value in w16_a[curr_student]:
+            w16_total += value.hours_attended
+
         overall_hours = m1_total+m2_total+m3_total+m4_total
 
         all_totals[curr_student] = [m1_total, m2_total, m3_total, m4_total, overall_hours]
+        week_totals[curr_student] = [w1_total, w2_total, w3_total, w4_total, w5_total, w6_total, w7_total, w8_total, w9_total, w10_total, w11_total, w12_total, w13_total, w14_total, w15_total, w16_total]
+        
+        m_grand_total[0] += m1_total
+        m_grand_total[1] += m2_total
+        m_grand_total[2] += m3_total
+        m_grand_total[3] += m4_total
+
+        w_grand_total[0] += w1_total
+        w_grand_total[1] += w2_total
+        w_grand_total[2] += w3_total
+        w_grand_total[3] += w4_total
+        w_grand_total[4] += w5_total
+        w_grand_total[5] += w6_total
+        w_grand_total[6] += w7_total
+        w_grand_total[7] += w8_total
+        w_grand_total[8] += w9_total
+        w_grand_total[9] += w10_total
+        w_grand_total[10] += w11_total
+        w_grand_total[11] += w12_total
+        w_grand_total[12] += w13_total
+        w_grand_total[13] += w14_total
+        w_grand_total[14] += w15_total
+        w_grand_total[15] += w16_total
+        
+        grand_total[0] = m_grand_total[0] + m_grand_total[1] + m_grand_total[2] + m_grand_total[3]
 
     context = {
         'course': course,
-        'm1_total': m1_total,
-        'm2_total': m2_total,
-        'm3_total': m3_total,
-        'm4_total': m4_total,
+        #'m1_total': m1_total,
+        #'m2_total': m2_total,
+        #'m3_total': m3_total,
+        #'m4_total': m4_total,
+        'm_grand_total': m_grand_total,
+        'w_grand_total': w_grand_total, 
+        'grand_total': grand_total,  
         'all_totals': all_totals,
+        'week_totals': week_totals,
         #'attendances': attendances.items(),
         'roster_entries': roster_entries,
         'students': students,
@@ -242,9 +391,12 @@ def attendance(request, course_id):
     return HttpResponse(template.render(context, request))
 
 @login_required
-def graphattendancebymonth(request, course_id, month_number):
+def graphattendancebymonth(request, student_id, course_id, month_number):
     course = Course.objects.get(pk=course_id)
+    student = Student.objects.get(pk=student_id)
     roster_entries = Roster.objects.filter(course=course)
+
+    time_period = "month " + month_number
 
     month_attendances = []
 
@@ -253,23 +405,28 @@ def graphattendancebymonth(request, course_id, month_number):
         month_number=month_number))
 
 
-    f_a_t = []
+    f_a_t = {}
+    f_a_t = [0,0,0,0]
 
     for item in month_attendances:
-        if item.attendance_mark == 'FULL':
-            f_a_t[0] += 1
+        for mark in item:
+            if mark.attendance_mark == 'FULL':
+                f_a_t[0] += 1
 
-        elif item.attendance_mark == 'TARDY':
-            f_a_t[3] += 1
+            elif mark.attendance_mark == 'TARDY':
+                f_a_t[3] += 1
 
-        elif item.attendance_mark == 'ABSENT' and item.absent_excused == True:
-            f_a_t[1] += 1
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == True:
+                f_a_t[1] += 1
 
-        elif item.attendance_mark == 'ABSENT' and item.absent_excused == False:
-            f_a_t[2] += 1
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == False:
+                f_a_t[2] += 1
 
     context = {
         'f_a_t': f_a_t,
+        'course': course,
+        'student': student,
+        'time_period': time_period,
     }
 
     template = loader.get_template('graph.html')
@@ -277,34 +434,130 @@ def graphattendancebymonth(request, course_id, month_number):
     return HttpResponse(template.render(context, request))
 
 @login_required
-def graphattendancebyweek(request, course_id, month_number, week_number):
+def graphattendancebyweek(request, student_id, course_id, week_number):
     course = Course.objects.get(pk=course_id)
+    student = Student.objects.get(pk=student_id)
     roster_entries = Roster.objects.filter(course=course)
+
+    time_period = "week " + week_number
 
     week_attendances = []
 
     for roster in roster_entries:
         week_attendances.append(Attendance.objects.filter(roster=roster,
-        month_number=month_number, week_number=week_number))
+         week_number=week_number))
 
 
-    f_a_t = []
+    f_a_t = {}
+    f_a_t = [0,0,0,0]
 
     for item in week_attendances:
-        if item.attendance_mark == 'FULL':
-            f_a_t[0] += 1
+        for mark in item:
+            if mark.attendance_mark == 'FULL':
+                f_a_t[0] += 1
 
-        elif item.attendance_mark == 'TARDY':
-            f_a_t[3] += 1
+            elif mark.attendance_mark == 'TARDY':
+                f_a_t[3] += 1
 
-        elif item.attendance_mark == 'ABSENT' and item.absent_excused == True:
-            f_a_t[1] += 1
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == True:
+                f_a_t[1] += 1
 
-        elif item.attendance_mark == 'ABSENT' and item.absent_excused == False:
-            f_a_t[2] += 1
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == False:
+                f_a_t[2] += 1
 
     context = {
         'f_a_t': f_a_t,
+        'course': course,
+        'student': student,
+        'time_period': time_period,
+    }
+
+    template = loader.get_template('graph.html')
+
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def graphattendanceoverall(request, student_id, course_id):
+    course = Course.objects.get(pk=course_id)
+    student = Student.objects.get(pk=student_id)
+    roster_entries = Roster.objects.filter(course=course)
+
+    time_period = "semester"
+
+    total_attendances = []
+
+    for roster in roster_entries:
+        total_attendances.append(Attendance.objects.filter(roster=roster))
+
+
+    f_a_t = {}
+    f_a_t = [0,0,0,0]
+
+    for item in total_attendances:
+        for mark in item:
+            if mark.attendance_mark == 'FULL':
+                f_a_t[0] += 1
+
+            elif mark.attendance_mark == 'TARDY':
+                f_a_t[3] += 1
+
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == True:
+                f_a_t[1] += 1
+
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == False:
+                f_a_t[2] += 1
+
+    context = {
+        'f_a_t': f_a_t,
+        'course': course,
+        'student': student,
+        'time_period': time_period,
+    }
+
+    template = loader.get_template('graph.html')
+
+    return HttpResponse(template.render(context, request))
+
+# this function is not yet implemented 
+@login_required
+def graphoverallmonth(request, course_id, month_number):
+    course = Course.objects.get(pk=course_id)
+    roster_entries = Roster.objects.filter(course=course)
+
+    time_period = "month " + month_number
+
+    month_attendances = []
+    temp_month = []
+
+    for roster in roster_entries:
+        month_attendances.append(Attendance.objects.filter(month_number=month_number))
+
+#    for data in temp_month:
+
+        #if data.roster.course == course:
+         #   month_attendances.append(data)
+
+    f_a_t = {}
+    f_a_t = [0,0,0,0]
+
+    for item in month_attendances:
+        for mark in item:
+            if mark.attendance_mark == 'FULL' and mark.roster.course == course:
+                f_a_t[0] += 1
+
+            elif mark.attendance_mark == 'TARDY' and mark.roster.course == course:
+                f_a_t[3] += 1
+
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == True and mark.roster.course == course:
+                f_a_t[1] += 1
+
+            elif mark.attendance_mark == 'ABSENT' and mark.absent_excused == False and mark.roster.course == course:
+                f_a_t[2] += 1
+
+    context = {
+        'f_a_t': f_a_t,
+        'course': course,
+        'time_period': time_period,
     }
 
     template = loader.get_template('graph.html')
